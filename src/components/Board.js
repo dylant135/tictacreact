5bc29b8fdb10900ba0b51deb58dc11500975bd07
dylan <dylangthomas1@gmail.com>
2022-11-04 12:00:12 -0600

    board setup

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,18 +1,17 @@
 import React from "react";
-import Square from './Square'
 
 export default function Board() {
     return (
-        <div>
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
+        <div className="container">
+            <div className="square" id='0'></div>
+            <div className="square" id='1'></div>
+            <div className="square" id='2'></div>
+            <div className="square" id='3'></div>
+            <div className="square" id='4'></div>
+            <div className="square" id='5'></div>
+            <div className="square" id='6'></div>
+            <div className="square" id='7'></div>
+            <div className="square" id='8'></div>
         </div>
     )
 }
